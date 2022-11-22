a0f400e3eecd021a6e7277a54f3f5c1d510bac16
Arkadiusz Zieliński <azielinski@digitalvirgo.com>
2022-11-22 03:18:44 +0100

    MAIN improvement: Setting starting point

---
+++ LinqTutorials/LinqTutorials/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task1Result = LinqTasks.Task1();
+        }
+    }
+}
